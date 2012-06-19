7512dddc7cd3db59e28f88d740ea50eafb411507
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-06-19 16:56:02 -0300

    A lot of fixes in views.py to make it work and avoid crashes.

--- hitcount/views.py
+++ hitcount/views.py
@@ -1,10 +1,11 @@
 from django.http import Http404, HttpResponse, HttpResponseBadRequest
 from django.utils import simplejson
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
+#from django.contrib.contenttypes.models import ContentType
 
 from hitcount.utils import get_ip
 from hitcount.models import Hit, HitCount, BlacklistIP, BlacklistUserAgent
+
 
 def _update_hit_count(request, hitcount):
     '''
@@ -16,12 +17,20 @@ def _update_hit_count(request, hitcount):
 
     Returns True if the request was considered a Hit; returns False if not.
     '''
-    user = request.user
-    session_key = request.session.session_key
+    try:
+        user = request.user
+    except:
+        user = None
+
+    try:
+        session_key = request.session.session_key
+    except:
+        session_key = None
+
     ip = get_ip(request)
     user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
     hits_per_ip_limit = getattr(settings, 'HITCOUNT_HITS_PER_IP_LIMIT', 0)
-    exclude_user_group = getattr(settings, 
+    exclude_user_group = getattr(settings,
                             'HITCOUNT_EXCLUDE_USER_GROUP', None)
 
     # first, check our request against the blacklists before continuing
@@ -30,12 +39,12 @@ def _update_hit_count(request, hitcount):
         return False
 
     # second, see if we are excluding a specific user group or not
-    if exclude_user_group and user.is_authenticated():
+    if exclude_user_group and user and user.is_authenticated():
         if user.groups.filter(name__in=exclude_user_group):
             return False
 
     #start with a fresh active query set (HITCOUNT_KEEP_HIT_ACTIVE )
-    qs = Hit.objects.filter_active() 
+    qs = Hit.objects.filter_active()
 
     # check limit on hits from a unique ip address (HITCOUNT_HITS_PER_IP_LIMIT)
     if hits_per_ip_limit:
@@ -43,21 +52,21 @@ def _update_hit_count(request, hitcount):
             return False
 
     # create a generic Hit object with request data
-    hit = Hit(  session=session_key,
-                hitcount=hitcount,
-                ip=get_ip(request),
-                user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],)
+    hit = Hit(session=session_key,
+              hitcount=hitcount,
+              ip=get_ip(request),
+              user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],)
 
     # first, use a user's authentication to see if they made an earlier hit
-    if user.is_authenticated():
-        if not qs.filter(user=user,hitcount=hitcount):
-            hit.user = user #associate this hit with a user
+    if user and user.is_authenticated():
+        if not qs.filter(user=user, hitcount=hitcount):
+            hit.user = user  # associate this hit with a user
             hit.save()
             return True
-
     # if not authenticated, see if we have a repeat session
     else:
-        if not qs.filter(session=session_key,hitcount=hitcount):
+        if session_key and \
+            not qs.filter(session=session_key, hitcount=hitcount):
             hit.save()
 
             # forces a save on this anonymous users session
@@ -65,11 +74,13 @@ def _update_hit_count(request, hitcount):
 
             return True
 
-    return False 
+    return False
+
 
 def json_error_response(error_message):
     return HttpResponse(simplejson.dumps(dict(success=False,
                                               error_message=error_message)))
+
 
 # TODO better status responses - consider model after django-voting,
 # right now the django handling isn't great.  should return the current
@@ -79,7 +90,7 @@ def update_hit_count_ajax(request):
     '''
     Ajax call that can be used to update a hit count.
 
-    Ajax is not the only way to do this, but probably will cut down on 
+    Ajax is not the only way to do this, but probably will cut down on
     bots and spiders.
 
     See template tags for how to implement.
@@ -93,18 +104,16 @@ def update_hit_count_ajax(request):
         return json_error_response("Hits counted via POST only.")
 
     hitcount_pk = request.POST.get('hitcount_pk')
-    
     try:
         hitcount = HitCount.objects.get(pk=hitcount_pk)
     except:
         return HttpResponseBadRequest("HitCount object_pk not working")
 
     result = _update_hit_count(request, hitcount)
-
     if result:
         status = "success"
     else:
         status = "no hit recorded"
 
     json = simplejson.dumps({'status': status})
-    return HttpResponse(json,mimetype="application/json")
+    return HttpResponse(json, mimetype="application/json")
